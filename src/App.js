43a6f3461f1b6d7cd8e0a539e8a84a0b84c1628a
Martin Matejovsky <matejovsky.ma@volny.cz>
2024-09-05 23:07:02 +0200

    Installed Tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
-
 function App() {
+  const addNewWord = () => {
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Trénujte vlastní sadu slovíček z cizího jazyka</h1>
+        <h1 className="text-center text-3xl font-bold underline">Trénujte vlastní sadu slovíček z cizího jazyka</h1>
+
+        <buton type="button" onClick={addNewWord} className="rounded">Vložit nová slova</buton>
       </header>
     </div>
   );
